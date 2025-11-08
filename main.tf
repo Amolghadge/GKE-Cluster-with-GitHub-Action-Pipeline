dd975905da4b7c6bf4a94df1c708935be31f781f
Amol Ghadge <64397760+Amolghadge@users.noreply.github.com>
2025-11-08 11:52:05 +0530

    Format GKE cluster resource properties

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "google" {
 
 # GKE cluster
 resource "google_container_cluster" "primary" {
-  name     = "${var.project_id}-gke"
-  location = var.region
+  name                     = "${var.project_id}-gke"
+  location                 = var.region
   
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
